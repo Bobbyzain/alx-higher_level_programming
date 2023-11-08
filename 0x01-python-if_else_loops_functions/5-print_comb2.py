46dd528f2b5b31c2f6b1ab2aea0c4293bdfb6ea0
Bobbyzain <abhameed419@gmail.com>
2023-11-08 00:19:26 -0800

    Hot fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 for i, num  in enumerate(range(100)):
     if i < 99:
-        if i < 10:
-            print("0{}".format(num), end=", ")
-        else:
-            print("{}".format(num), end=", ")
+        print("{:02d}".format(num), end=", ")
     else:
         print("{}".format(num))

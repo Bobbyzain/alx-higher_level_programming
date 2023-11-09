d211f619cddf0b4fc348c43b2bc2af198f44b767
Bobbyzain <abhameed419@gmail.com>
2023-11-08 20:19:32 -0800

    Hot fix

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if type(number) != int:
-        return None
+        raise ValueError()
     c = str(number)
     print("{}".format(c[-1]), end='')
     return c[-1]

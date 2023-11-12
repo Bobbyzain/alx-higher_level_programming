5de76ba899e56f246f3901fd4604afb6369a4046
Bobbyzain <abhameed419@gmail.com>
2023-11-12 14:07:48 -0800

    Hot fix

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in my_list:
-        print("{}".format(i))
+        if type(i) != int:
+            pass
+        else:
+            print("{}".format(i))

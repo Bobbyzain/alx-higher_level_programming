27ac561eb3abd3ac98140a3db67d8b35907e4c4d
Bobbyzain <abhameed419@gmail.com>
2023-11-13 00:24:37 -0800

    Reversing the content of a list

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if len(my_list) > 0:
+        j = len(my_list) - 1
+        while j >= 0:
+            # if type(i) == int:
+            if type(my_list[j]) == float:
+                print("{:.0f}".format(my_list[j]))
+            else:
+                print("{:d}".format(my_list[j]))
+            j -= 1

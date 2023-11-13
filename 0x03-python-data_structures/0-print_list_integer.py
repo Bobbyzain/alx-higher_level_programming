c1840abf0544bfe1397763316b4f46d42fc9a4c9
Bobbyzain <abhameed419@gmail.com>
2023-11-12 23:49:12 -0800

    Hot fix

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,7 +2,8 @@
 def print_list_integer(my_list=[]):
     if len(my_list) > 0:
         for i in my_list:
-            if type(i) == int:
-                print("{:d}".format(i))
-            elif type(i) == float:
+            # if type(i) == int:
+            if type(i) == float:
                 print("{:.0f}".format(i))
+            else:
+                print("{:d}".format(i))

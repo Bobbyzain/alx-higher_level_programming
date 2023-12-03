45a4486e429a7d8fc83b0d923e5dc4efdb804feb
Bobbyzain <abhameed419@gmail.com>
2023-12-03 15:06:26 -0800

    Changing an element in a list without altering the original list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    g = my_list.copy()
+    if idx < 0:
+        return g
+    elif idx > len(my_list):
+        return g
+    else:
+        g[idx] = element
+ 
+    return g

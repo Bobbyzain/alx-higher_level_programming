ac935fca45185db9f2f81af1740bceae0ff7d7cd
Bobbyzain <abhameed419@gmail.com>
2024-01-11 06:22:39 -0800

    Hot fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
-class Square():
-    """ This is an empty class used to deine a square """
+""" My first class ever!!!.
+
+This module provides the definition of a  square
+"""
+
+
+class Square:
+    """ This is an empty class used to deine a square
+
+    def __init__(self):
+        My init method for defining a square
+        Args:
+            sides (int): This is the number of sides possessed by the square
+        self.sides =  sides #: Attribute definition
+        if self.sides == 4:
+            print("A square {} sides".format(self.sides))
+        else:
+            print("A square actually has 4 sides not {}".format(self.sides))
+    """
     pass

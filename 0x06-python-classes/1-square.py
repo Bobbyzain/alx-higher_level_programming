115e806b024c9ee71234d560225c8fb0c75e7c4d
Bobbyzain <abhameed419@gmail.com>
2024-01-11 06:58:41 -0800

    a class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" My first class ever!!!.
+
+This module provides the definition of a  square
+"""
+
+
+class Square:
+    """ This is an empty class used to deine a square
+
+    This square will be built with subsequent tasks in this project
+    """
+    def __init__(self, size):
+        """My init method for defining a square
+        Args:
+            sides (int): This is the number of sides possessed by the square
+        """
+        self._size = size  #: Attribute definition
+        """if self.size == 4:
+            print("A square {} sides".format(self.size))
+        else:
+            print("A square actually has 4 sides not {}".format(self.sides))
+        """
